d899fb5ad9d972ba43e417988f8d46436141973c
Nilufergul <nilufergulciftci03@gmail.com>
2024-12-02 16:44:44 +0300

    new messages_controller.rb

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,28 +1,24 @@
 class MessagesController < ApplicationController
-    #before_action :require_user
-  
-    def create
-      user = User.find(1) # login_logout kısmından sonra değişecek
-  
-      message = user.messages.build(message_params)
-  
-      if message.save
-        ActionCable.server.broadcast "chatroom_channel",
-                                      mod_message: message_render(message)
+  #before_action :require_user
 
-      end
-    end
-  
-    private
-  
-    def message_params
-      params.require(:message).permit(:body)
-    end
+  def create
+    message = current_user.messages.build(message_params)
+    if message.save
+      ActionCable.server.broadcast "chatroom_channel",
+                                    mod_message: message_render(message)
 
-    def message_render(message)
-      render(partial: 'message', locals: {message: message})
     end
-
-    
   end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:body)
+  end
+
+  def message_render(message)
+    render(partial: 'message', locals: {message: message})
+  end
+
   
+end
