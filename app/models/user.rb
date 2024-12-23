39f9b24f17f7b779291e049725c2a03586b9762c
Şakir Yılmaz Öğüt <zimowcs@gmail.com>
2024-12-24 00:12:55 +0300

    Mail extension check for register, fix send message by pressing Enter

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,18 @@
 class User < ApplicationRecord
-    has_secure_password
-    has_many :messages
-  
-    validates :name, presence: true
-    validates :username, presence: true, uniqueness: true
-    validates :email, presence: true, uniqueness: true
+  has_secure_password
+  has_many :messages
+
+  validates :name, presence: true
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validate :email_domain_validation
+
+  private
+
+  def email_domain_validation
+    allowed_domains = ['@live.acibadem.edu.tr', '@acibadem.edu.tr']
+    unless allowed_domains.any? { |domain| email.end_with?(domain) }
+      errors.add(:email, 'must be from an allowed domain (e.g., @live.acibadem.edu.tr, @acibadem.edu.tr)')
+    end
   end
+end
