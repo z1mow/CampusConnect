3fccefdabc53aaaebf633441312c9ca227b6a889
Şakir Yılmaz Öğüt <zimowcs@gmail.com>
2024-12-24 17:13:27 +0300

    application.js renewed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,7 @@
+import "channels";
 import Rails from "@rails/ujs";
 import Turbolinks from "turbolinks";
 import * as ActiveStorage from "@rails/activestorage";
-import "channels"; // Kanalları dahil ediyor
 
 Rails.start();
 Turbolinks.start();
