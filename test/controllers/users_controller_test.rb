44d4195344aab3ce1e223462dc0fccd2e1ce880f
Umut-K <32umutkk@gmail.com>
2024-12-30 23:14:00 +0300

    test/fixture fixed

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,12 +2,25 @@ require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get users_new_url
+    get new_user_url
     assert_response :success
   end
 
   test "should get create" do
-    get users_create_url
-    assert_response :success
+    assert_difference('User.count') do
+      post users_url, params: { user: {
+        name: "New Test User",
+        username: "newtestuser",
+        email: "newtest@acibadem.edu.tr",
+        password: "password123",
+        password_confirmation: "password123"
+      }}
+      if response.status == 422
+        user = User.new(name: "New Test User", username: "newtestuser", email: "newtest@acibadem.edu.tr", password: "password123", password_confirmation: "password123")
+        user.valid?
+        puts "Validation errors: #{user.errors.full_messages}"
+      end
+    end
+    assert_redirected_to root_url
   end
 end
